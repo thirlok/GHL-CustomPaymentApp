b725915a0dae492c46d72ee777926afc0de0dc70
Rajeshkumar0110 <rajeshkumar@digisource.co.in>
2025-11-03 11:58:25 +0530

    removed unwanted imports in app file

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import CustomPage from "./Pages/customPage";
 import HPS3 from "./Pages/HPS3/HPS3";
 import Home from "./Home";
 import CreatePayment from "./Pages/createPayment";
-import QueryPage from "./iframseTest/QueryPage";
-import GhlPage from "./iframseTest/GHLpage";
-import Latpaypage from "./iframseTest/Latpaypage";
+// import QueryPage from "./iframseTest/QueryPage";
+// import GhlPage from "./iframseTest/GHLpage";
+// import Latpaypage from "./iframseTest/Latpaypage";
 import GhlAuth from "./ghlAuth";
 
 function App() {
@@ -17,14 +17,14 @@ function App() {
       
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/ghlpage" element={<GhlPage />} />
-          <Route path="/latpaypage" element={<Latpaypage/>}/>
+          {/* <Route path="/ghlpage" element={<GhlPage />} /> */}
+          {/* <Route path="/latpaypage" element={<Latpaypage/>}/> */}
           <Route path="/ghlauth" element={<GhlAuth />} />
           <Route path="/ghlconfig" element={<GhlTestPage />} />
           <Route path="/custom-page" element={<CustomPage />} />
           <Route path="/hps3" element={<HPS3 />} />
           <Route path="/createpayment" element={<CreatePayment />} />
-          <Route path="/querypage" element={<QueryPage />} />
+          {/* <Route path="/querypage" element={<QueryPage />} /> */}
         </Routes>
     
       </Router>
